e3c47f89b153157423ce887cd44b37f29ce82fd8
nmmz76 <nmmz76@mst.edu>
2019-05-12 16:57:35 -0500

    sparse

---
+++ SparseMultiEvoMunkres.py
@@ -0,0 +1,327 @@
+import numpy as np
+from multiprocessing import Pool
+from munkres import Munkres
+import matplotlib.pyplot as plt
+import itertools
+from sparse_dict import sparse_dict
+
+def validate(matching):
+    cols = matching.sum(0)
+    rows = matching.sum(1)
+    # print(cols.shape)
+    n_cols = cols.sum()
+    n_rows = rows.sum()
+    return np.array_equal((n_rows,n_cols),matching.shape)
+
+def common_edges(A,B):
+    return A.logical_and(B)
+
+def complete_matching(_matching):
+    matching = _matching.copy()
+    cols = matching.any(0)
+    rows = matching.any(1)
+    # print(cols.shape)
+    n_cols = np.sum(np.logical_not(cols))
+    n_rows = np.sum(np.logical_not(rows))
+    n = np.minimum(n_rows,n_cols)
+    match_cols = np.random.permutation(n_cols)
+    match_rows = np.random.permutation(n_rows)
+    if n < n_cols:
+        match_cols = np.choose(match_cols,n)
+    if n < n_rows:
+        match_rows = np.choose(match_rows,n)
+    for ci, col in enumerate(cols):
+        if col:
+            match_cols[match_cols >= ci] += 1
+
+    for ri, row in enumerate(rows):
+        if row:
+            match_rows[match_rows >= ri] += 1
+
+    for mr, mc in zip(match_rows, match_cols):
+        matching.set(mr,mc,1)
+    if not validate(matching):
+        print('Invalid Matching')
+        exit()
+    return matching
+
+def mutate(_matching, p=0.01):
+    matching = _matching.copy()
+    # print('s',matching.shape)
+    n = np.max(matching.shape)
+    flips = np.random.random(n) < p
+    if matching.shape[0] >= matching.shape[1]:
+        matching[flips,:] = 0
+    else:
+        matching[:,flips] = 0
+    return complete_matching(matching)
+
+def mate(A,B,m_rate=0.01):
+    return mutate(common_edges(A,B),p=m_rate)
+
+def breed(A,B,m_rate=0.01,n_offspring=1):
+    if n_offspring == 1:
+        return mate(A,B,m_rate)
+    else:
+        return [mate(A,B,m_rate) for _ in range(n_offspring)]
+
+def pbreed(args):
+    return breed(*args)
+
+def pmate(args):
+    return mate(*args)
+
+def cross_breed_population(population,fitness1, fitness2,num_to_gen,m_rate=0.01):
+    nfit1 = fitness1/np.sum(fitness1)
+    nfit2 = fitness2/np.sum(fitness2)
+    nfit3 = np.add(fitness1,fitness2)/2
+    nfit3 /= np.sum(nfit3)
+    idx = list(range(len(population)))
+    num_to_gen1 = int(num_to_gen/2)
+    num_to_gen2 = num_to_gen-num_to_gen1
+    parent_idx = [(np.random.choice(idx,p=nfit1),np.random.choice(idx,p=nfit2)) for _ in range(num_to_gen1)]
+    parent_idx += [(np.random.choice(idx,p=nfit3),np.random.choice(idx,p=nfit3)) for _ in range(num_to_gen2)]
+    parent_pairs = [(population[i],population[j],m_rate) for i,j in parent_idx]
+    # p = Pool(1)
+    # next_gen = list(p.map(pmate,parent_pairs))
+    # p.close()
+    next_gen = [pmate(a) for a in parent_pairs]
+    return next_gen
+
+def evaluate(matching, weights):
+    return weights[matching].sum()
+
+def pevaluate(args):
+    return evaluate(*args)
+
+def breed_and_evaluate(population, fitness, weights, num_to_gen, m_rate=0.01):
+    nw = len(weights)
+    all_combos = list(itertools.combinations(list(range(nw)), 2))
+    num_to_gen_per = int(num_to_gen / (len(all_combos) + nw))
+    children = []
+    for i,j in all_combos:
+        children += cross_breed_population(population,fitness[i],fitness[j],num_to_gen_per,m_rate)
+    for i in range(nw):
+        children += cross_breed_population(population, fitness[i], fitness[i], num_to_gen_per, m_rate)
+    child_fitness = []
+    for i in range(nw):
+        child_fitness.append([evaluate(c,weights[i]) for c in children])
+    child_fit = list(zip(*child_fitness,children))
+    return child_fit
+
+def cull(population, fitness, pop_limit):
+    nw = len(fitness)
+    npop_limit = min(len(population),pop_limit)
+    idx = list(range(len(population)))
+    fitness.append([np.mean(x) for x in zip(*fitness)])
+    pop_fit = list(zip(*fitness, population, idx))
+    pop_fits = [None for _ in range(nw+1)]
+    for i in range(nw+1):
+        pop_fit.sort(reverse=True,key=lambda x: x[i])
+        pop_fits[i] = list(pop_fit)
+    npop_fit = []
+    npop_fit_idx = []
+    i = 0
+    j = 0
+    while len(npop_fit) < npop_limit:
+        if pop_fits[j][i][-1] not in npop_fit_idx:
+            npop_fit.append(pop_fits[j][i][:-1])
+            npop_fit_idx.append(pop_fits[j][i][-1])
+        j = -1
+        i += 1
+    *nfitness, npopulation = list(map(list, zip(*npop_fit)))
+    return nfitness[:-1], npopulation
+
+
+def spawn_gen(pop_size,weights1):
+    population = [complete_matching(np.zeros_like(weights1,dtype=bool)) for _ in range(pop_size)]
+    fitness1 = np.array([evaluate(p,weights1) for p in population])
+    return fitness1, population
+
+def breed_and_cull(population, fitness, weights,population_size, keep_top_num,birthrate, m_rate):
+    nw = len(weights)
+    idx = list(range(len(population)))
+    pop_fit = list(zip(*fitness, population, idx))
+    pop_fit.sort(reverse=True, key=lambda x: x[0])
+
+    keep_top_num_per = int(keep_top_num/nw)
+
+    next_gen = pop_fit[:keep_top_num_per]
+    next_gen_idx = [x[-1] for x in next_gen]
+    for i in range(1,nw):
+        pop_fit.sort(reverse=True, key=lambda x: x[i])
+        ngi = 0
+        while len(next_gen) < min((i+1)*keep_top_num_per,keep_top_num):
+            if pop_fit[ngi][-1] not in next_gen_idx:
+                next_gen.append(pop_fit[ngi])
+                next_gen_idx.append(pop_fit[ngi][-1])
+            ngi += 1
+    next_gen = [x[:-1] for x in next_gen]
+    # mutated_next_gen = [mutate(ng,m_rate) for _,_,ng in next_gen for _ in range(3)]
+    # next_gen += [(evaluate(ng,weights1),evaluate(ng,weights2),ng) for ng in mutated_next_gen]
+    next_gen += breed_and_evaluate(population, fitness, weights, int(population_size * birthrate), m_rate)
+    *next_gen_fitness, next_gen_population = list(map(list, zip(*next_gen)))
+    fitness, population = cull(next_gen_population, next_gen_fitness, population_size)
+    return fitness, population
+
+def random_breed(population, fitness, weights,population_size, keep_top_num,birthrate, m_rate):
+    idx = list(range(len(population)))
+    pop_fit = list(zip(*fitness, population, idx))
+    nw = len(weights)
+    next_gen_fitness, next_gen_population = cull(population,fitness,keep_top_num)
+    next_gen = list(zip(*next_gen_fitness, next_gen_population))
+    gf = [[] for _ in range(nw)]
+    gf[0],gp = spawn_gen(int(population_size * birthrate), weights[0])
+    for i in range(1,nw):
+        gf[i] = [evaluate(p, weights[i]) for p in gp]
+    next_gen += list(zip(*gf,gp))
+    *next_gen_fitness, next_gen_population = list(map(list, zip(*next_gen)))
+    fitness, population = cull(next_gen_population, next_gen_fitness, population_size)
+    return fitness, population
+
+
+def MultiEvoMunkres(*weights,generations,population_size,birthrate=2.0,keep_top_num=1,m_rate=0.01):
+    nw = len(weights)
+    history = [[] for _ in range(nw)]
+    pop_history = []
+    fitness = [[] for _ in range(nw)]
+    fitness[0], population = spawn_gen(population_size, weights[0])
+    for i in range(1,nw):
+        fitness[i] = [evaluate(p, weights[i]) for p in population]
+    no_change_count = [0 for _ in range(nw)]
+    dm_rate = m_rate
+    last_fitness = [0 for _ in range(nw)]
+    for gen in range(generations):
+        pop_history.append(fitness)
+        top_indvs = get_top_individuals(fitness, population)
+        # print('Gen {}, Top: {}'.format(gen, top_indvs))
+        for i in range(nw):
+            history[i].append(top_indvs[i][:nw])
+        for i in range(nw):
+            if top_indvs[i][i] == last_fitness[i]:
+                no_change_count[i] += 1
+            else:
+                no_change_count[i] =0
+                dm_rate = m_rate
+        if np.any(np.greater(no_change_count,2)):
+            dm_rate = min(0.15, dm_rate + 0.005)
+            no_change_count = [0 for _ in range(nw)]
+        for i in range(nw):
+            last_fitness[i] = top_indvs[i][i]
+        # print('Mutation Rate: {}'.format(dm_rate))
+
+        fitness, population = breed_and_cull(population, fitness, weights, population_size, keep_top_num, birthrate, m_rate)
+    top_indvs = get_top_individuals(fitness, population)
+    pop_history.append(fitness)
+    return top_indvs[0], history, pop_history
+
+def get_top_individuals(fitness, population):
+    nw = len(fitness)
+    pop_fit = list(zip(*fitness, population))
+    top_indvs = []
+    for i in range(nw):
+        pop_fit.sort(reverse=True, key=lambda x: x[i])
+        top_indvs.append(pop_fit[i])
+    return top_indvs
+
+
+def MultiRandMunkres(*weights,generations,population_size,birthrate=2.0,keep_top_num=1,m_rate=0.01):
+    nw = len(weights)
+    history = [[] for _ in range(nw)]
+    pop_history = []
+    fitness = [[] for _ in range(nw)]
+    fitness[0], population = spawn_gen(population_size, weights[0])
+    for i in range(1, nw):
+        fitness[i] = [evaluate(p, weights[i]) for p in population]
+
+    for gen in range(generations):
+        pop_history.append(list(fitness))
+        top_indvs = get_top_individuals(fitness, population)
+        # print('Gen {}, Top: {}'.format(gen,top_indvs))
+        for i in range(nw):
+            history[i].append(top_indvs[i][:nw])
+        fitness, population = random_breed(population, fitness, weights, population_size, keep_top_num, birthrate, m_rate)
+    pop_fit1 = list(zip(*fitness, population))
+    pop_fit1.sort(reverse=True, key=lambda x: x[0])
+    top_indv = pop_fit1[0]
+    pop_history.append(fitness)
+    return top_indv, history, pop_history
+
+def find_and_eval_matching(weights1,weights2=None):
+    m = Munkres()
+    matching = m.compute(np.copy(weights1))
+    matching_weight1 = []
+    matching_weight2 = []
+    match_mat = np.zeros_like(weights1, dtype=bool)
+    for j, r in matching:
+        matching_weight1.append(weights1[j, r])
+        if weights2 is not None:
+            matching_weight2.append(weights2[j, r])
+        match_mat[j, r] = 1
+    if weights2 is None:
+        return matching, matching_weight1, match_mat
+    else:
+        return matching, matching_weight1, matching_weight2, match_mat
+
+def find_matching(weights1):
+    nweights1 = np.max(weights1) - weights1
+    m = Munkres()
+    matching = m.compute(np.copy(nweights1))
+    match_mat = np.zeros_like(weights1, dtype=bool)
+    for j, r in matching:
+        match_mat[j, r] = 1
+    return match_mat
+
+if __name__ == '__main__':
+    weights1 = np.random.random((4,4))
+    weights2 = np.random.random((4,4))
+    nweights1 = np.max(weights1)-weights1
+    _,_,oM = find_and_eval_matching(nweights1)
+    sweights1 = sparse_dict(0,0)
+    sweights2 = sparse_dict(0,0)
+    OM = sparse_dict(0,0)
+    sweights1.fromarray(weights1)
+    sweights2.fromarray(weights2)
+    OM.fromarray(oM)
+
+    soptimal1 = evaluate(oM,weights1)
+    optimal1 = evaluate(OM,sweights1)
+    optimal2 = evaluate(OM,sweights2)
+    print('Optimal: {}, {}'.format(optimal1, optimal2))
+    print(soptimal1)
+    # match, history, pop_history = MultiRandMunkres(weights1,weights2,generations=300,population_size=30,birthrate=30.0,keep_top_num=10,m_rate=0.05)
+    match, history, pop_history = MultiEvoMunkres(sweights1,sweights2,generations=300,population_size=30,birthrate=40.0,keep_top_num=3,m_rate=0.05)
+    print('Optimal: {}, {}'.format(optimal1, optimal2))
+    new_optimal1 = evaluate(match[-1],sweights1)
+    new_optimal2 = evaluate(match[-1],sweights2)
+    prcnt_f1 = (new_optimal1-optimal1)/optimal1
+    prcnt_f2 = (new_optimal2-optimal2)/optimal2
+    print('New Optimal: {}, {}'.format(new_optimal1, new_optimal2))
+    print('dF1: {}%, dF2: {}%'.format(prcnt_f1,prcnt_f2))
+
+    print(len(history))
+    print(len(history[0]))
+    print(len(history[0][0]))
+    h1, h2 = history
+    h11, h12 = list(map(list, zip(*h1)))
+    h21, h22 = list(map(list, zip(*h2)))
+    x = list(range(len(h1)))
+    plt.plot(x, h11, 'g--', label='Highest F1, F1')
+    plt.plot(x, h12, 'g-.', label='Highest F1, F2')
+    plt.plot(x, h21, 'm--', label='Highest F2, F1')
+    plt.plot(x, h22, 'm-.', label='Highest F2, F2')
+    plt.plot([0, len(h1)], [optimal1, optimal1], 'r--', label='Optimal F1')
+    plt.plot([0, len(h1)], [optimal2, optimal2], 'b--', label='Sub-Optimal F2')
+    plt.legend()
+    plt.title('Vector Evaluated Genetic Algorithm')
+    plt.xlabel('Iterations')
+    plt.ylabel('Fitness')
+    plt.show()
+    plt.show()
+
+
+
+
+
+
+
---
+++ sparse_dict.py
@@ -0,0 +1,249 @@
+import numpy as np
+
+class sparse_dict():
+    def __init__(self,mx=0,my=0):
+        self.mx = mx
+        self.my = my
+        self.shape = (mx,my)
+        self.data = dict()
+
+    def fill(self,data,rows,cols):
+        for d,r,c in zip(data,rows,cols):
+            self.set(r,c,d)
+
+    def set(self,x,y,d):
+        if d == 0:
+            self.rem(x,y)
+        elif x in self.data:
+            self.data[x][y] = d
+        else:
+            self.data[x] = {y:d}
+
+    def __setitem__(self, key, value):
+        x,y = key
+        if not isinstance(x,slice):
+            if not isinstance(y,slice):
+                self.set(x,y,value)
+            else:
+                for di, yi in enumerate(range(y.start,min(self.my,y).stop,y.step)):
+                    if isinstance(value, np.array):
+                        self.set(x,yi,value[di])
+                    elif value != 0:
+                        self.set(x, yi, value)
+                    else:
+                        self.rem(x,y)
+        elif not isinstance(y,slice):
+            for di, xi in enumerate(range(x.start, min(x.stop,self.mx), x.step)):
+                if isinstance(value, np.array):
+                    self.set(xi,y,value[di])
+                elif value != 0:
+                    self.set(xi, y, value)
+                else:
+                    self.rem(x, y)
+        else:
+            for di, xi in enumerate(range(x.start, min(x.stop, self.mx), x.step)):
+                for dj, yi in enumerate(range(y.start, min(self.my, y).stop, y.step)):
+                    if isinstance(value,np.array):
+                        self.set(xi,yi,value[di,dj])
+                    elif value != 0:
+                        self.set(xi, yi, value)
+                    else:
+                        self.rem(xi,yi)
+
+    def __getitem__(self, item):
+        print(type(item),item)
+        if isinstance(item,sparse_dict):
+            out = []
+            for x in item.data:
+                if x in self.data:
+                    for y in item.data[x]:
+                        if y in self.data[x]:
+                            out.append(self.data[x][y])
+                            # out.set(x,y,self.data[x][y])
+            return np.array(out)
+        x,y = item
+        if isinstance(x,slice):
+            if x.start is None:
+                x_start = 0
+            else:
+                x_start = x.start
+            if x.stop is None:
+                x_stop = self.mx
+            else:
+                x_stop = x.stop
+            if x.step is None:
+                x_step = 1
+            else:
+                x_step = x.step
+        if isinstance(y,slice):
+            if y.start is None:
+                y_start = 0
+            else:
+                y_start = y.start
+            if y.stop is None:
+                y_stop = self.my
+            else:
+                y_stop = y.stop
+            if y.step is None:
+                y_step = 1
+            else:
+                y_step = y.step
+        if not isinstance(x,slice):
+            if not isinstance(y,slice):
+                if x in self.data and y in self.data[x]:
+                    return self.data[x][y]
+                else:
+                    return 0
+            else:
+                out = sparse_dict(1,self.shape[1])
+                for yi in range(y_start,y_stop,y_step):
+                    if x in self.data and yi in self.data[x]:
+                        out.set(0,yi,self.data[x][yi])
+                return out
+
+        elif not isinstance(y,slice):
+            out = sparse_dict(self.shape[0],1)
+            for xi in range(x_start, x_stop, x_step):
+                if xi in self.data and y in self.data[x]:
+                    out.set(xi, 0, self.data[xi][y])
+            return out
+        else:
+            if x.step is None:
+                mx = int((x.stop-x.start))
+            else:
+                mx = int((x.stop-x.start)/x.step)
+            if y.step is None:
+                my = int((y.stop-y.start))
+            else:
+                my = int((y.stop-y.start)/y.step)
+            out = sparse_dict(mx,my)
+            for i, xi in enumerate(range(x_start, x_stop, x_step)):
+                for j,yi in enumerate(range(y_start,y_stop,y_step)):
+                    out[i,j] = self.data[xi][yi]
+            return out
+
+    def logical_and(self,other):
+        if self.mx != other.mx or self.my != other.my:
+            raise ValueError('Sparse matrices must share sizes')
+        common = sparse_dict(self.mx,self.my)
+        for x in self.data:
+            if x in other.data:
+                for y in self.data[x]:
+                    if y in other.data[x]:
+                        common.set(x,y,1.)
+        return common
+
+    def sum(self,axis=-1):
+        if axis == 0:
+            s = sparse_dict(1,self.my)
+            for x in self.data:
+                for y in self.data[x]:
+                    if s.data and y in s.data[0]:
+                        s.data[0][y] += 1
+                    else:
+                        s.set(0,y,1.)
+        else:
+            s = sparse_dict(self.mx, 1)
+            for x in self.data:
+                s.set(x, 0, sum(self.data[x].values()))
+            if axis != 1:
+                return sum([y for x in s.data.values() for y in x.values()])
+        return s
+
+
+    def any(self,axis):
+        if axis == 0:
+            a = np.zeros(self.my,dtype=bool)
+            for x in self.data:
+                for y in self.data[x]:
+                    a[y] = 1
+        else:
+            a = np.zeros(self.mx,dtype=bool)
+            for x in self.data:
+                a[x] = 1
+        return a
+
+    def rem(self,x,y):
+        if x in self.data:
+            if y in self.data[x]:
+                del self.data[x][y]
+            if not self.data[x]:
+                del self.data[x]
+
+    def todense(self):
+        dense = np.zeros(self.shape)
+        for x in self.data:
+            for y in self.data[x]:
+                dense[x,y] = self.data[x][y]
+        return dense
+
+    def copy(self):
+        cp = sparse_dict(*self.shape)
+        cp.data = dict(self.data)
+        return cp
+
+    def fromarray(self,a):
+        if isinstance(a,list):
+            a = np.array(a).reshape((-1,1))
+        d = []
+        r = []
+        c = []
+        for i in range(a.shape[0]):
+            for j in range(a.shape[1]):
+                if a[i, j]:
+                    d.append(a[i,j])
+                    r.append(i)
+                    c.append(j)
+        self.shape = a.shape
+        self.mx = self.shape[0]
+        self.my = self.shape[1]
+        self.fill(d, r, c)
+        return
+
+
+def random_sparse(shape):
+    a = np.random.random(shape)
+    a = a > 0.6
+    sd = sparse_dict(0,0)
+    sd.fromarray(a)
+    return sd, a
+
+if __name__ == '__main__':
+    s = (5,7)
+    a,aa = random_sparse(s)
+    b,bb = random_sparse(s)
+
+
+    print('sum 0')
+    print(a.sum(0).todense())
+    print(aa.sum(0))
+    print('sum 1')
+    print(a.sum(1).todense())
+    print(aa.sum(1))
+    print('sum -1')
+    print(a.sum())
+    print(aa.sum())
+    print('any 0')
+    print(a.any(0))
+    print(aa.any(0))
+    print('any 2')
+    print(a.any(1))
+    print(aa.any(1))
+    c = a.logical_and(b)
+    cd = c.todense()
+    print('LOG AND',np.array_equal(cd,np.logical_and(aa,bb)))
+    print('here')
+    d = a[b]
+    dd = aa[bb]
+    print('there')
+    print(np.array_equal(a.todense(),aa))
+    print(np.array_equal(b.todense(),bb))
+    print(dd.shape)
+    print(d.sum(),dd.sum())
+
+
+
+
+
+
+
